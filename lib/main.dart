84b76e7996255e3af46d46b897806912951765df
GabrielNeugebauer <gabrielneugebauer@hotmail.com>
2025-06-09 11:58:32 -0300

    Added Interface and basic func. Needs to add a clear functionality and the difficulty change.

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,261 @@
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Jogo2048App());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class Jogo2048App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Jogo 2048',
+      home: Jogo2048(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Jogo2048 extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _Jogo2048State createState() => _Jogo2048State();
 }
+class _Jogo2048State extends State<Jogo2048> {
+  @override
+  int moveCount = 0;
+  int gridSize = 4;
+  List<List<int>> grid = [];
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void moveLeft() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      bool moved = false;
+      for (int i = 0; i < gridSize; i++) {
+        List<int> row = grid[i].where((val) => val != 0).toList();
+        for (int j = 0; j < row.length - 1; j++) {
+          if (row[j] == row[j + 1]) {
+            row[j] *= 2;
+            row[j + 1] = 0;
+            moved = true;
+          }
+        }
+        row = row.where((val) => val != 0).toList();
+        while (row.length < gridSize) {
+          row.add(0);
+        }
+        if (!ListEquality().equals(grid[i], row)) moved = true;
+        grid[i] = row;
+      }
+      if (moved) {
+        moveCount++;
+        addNewTile();
+      }
     });
   }
 
+  void moveRight() {
+    setState(() {
+      bool moved = false;
+      for (int i = 0; i < gridSize; i++) {
+        List<int> row = grid[i].where((val) => val != 0).toList().reversed.toList();
+        for (int j = 0; j < row.length - 1; j++) {
+          if (row[j] == row[j + 1]) {
+            row[j] *= 2;
+            row[j + 1] = 0;
+            moved = true;
+          }
+        }
+        row = row.where((val) => val != 0).toList();
+        while (row.length < gridSize) {
+          row.add(0);
+        }
+        row = row.reversed.toList();
+        if (!ListEquality().equals(grid[i], row)) moved = true;
+        grid[i] = row;
+      }
+      if (moved) {
+        moveCount++;
+        addNewTile();
+      }
+    });
+  }
+
+  void moveUp() {
+    setState(() {
+      bool moved = false;
+      for (int j = 0; j < gridSize; j++) {
+        List<int> col = [];
+        for (int i = 0; i < gridSize; i++) {
+          if (grid[i][j] != 0) col.add(grid[i][j]);
+        }
+        for (int i = 0; i < col.length - 1; i++) {
+          if (col[i] == col[i + 1]) {
+            col[i] *= 2;
+            col[i + 1] = 0;
+            moved = true;
+          }
+        }
+        col = col.where((val) => val != 0).toList();
+        while (col.length < gridSize) {
+          col.add(0);
+        }
+        for (int i = 0; i < gridSize; i++) {
+          if (grid[i][j] != col[i]) moved = true;
+          grid[i][j] = col[i];
+        }
+      }
+      if (moved) {
+        moveCount++;
+        addNewTile();
+      }
+    });
+  }
+
+  void moveDown() {
+    setState(() {
+      bool moved = false;
+      for (int j = 0; j < gridSize; j++) {
+        List<int> col = [];
+        for (int i = gridSize - 1; i >= 0; i--) {
+          if (grid[i][j] != 0) col.add(grid[i][j]);
+        }
+        for (int i = 0; i < col.length - 1; i++) {
+          if (col[i] == col[i + 1]) {
+            col[i] *= 2;
+            col[i + 1] = 0;
+            moved = true;
+          }
+        }
+        col = col.where((val) => val != 0).toList();
+        while (col.length < gridSize) {
+          col.add(0);
+        }
+        for (int i = gridSize - 1, k = 0; i >= 0; i--, k++) {
+          if (grid[i][j] != col[k]) moved = true;
+          grid[i][j] = col[k];
+        }
+      }
+      if (moved) {
+        moveCount++;
+        addNewTile();
+      }
+    });
+  }
+
+  void initState() {
+    super.initState();
+    resetGame();
+  }
+  void resetGame() {
+    moveCount = 0;
+    grid = List.generate(gridSize, (_) => List.filled(gridSize, 0));
+    addNewTile();
+    addNewTile();
+  }
+  void addNewTile() {
+    List<int> emptyTiles = [];
+    for (int i = 0; i < gridSize; i++) {
+      for (int j = 0; j < gridSize; j++) {
+        if (grid[i][j] == 0) {
+          emptyTiles.add(i * gridSize + j);
+        }
+      }
+    }
+    if (emptyTiles.isNotEmpty) {
+      int randomIndex = emptyTiles[(emptyTiles.length * 0.5).floor()];
+      grid[randomIndex ~/ gridSize][randomIndex % gridSize] = 2;
+    }
+  }
+  // Draw interface
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double viewsize = View.of(context).physicalSize.aspectRatio;
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Jogo 2048'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: resetGame,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Movimentos: $moveCount', style: TextStyle(fontSize: 24)),
+          SizedBox(height: 20),
+          Expanded(
+            child: GridView.builder(
+
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: gridSize,
+                childAspectRatio: viewsize/.45,
+              ),
+              itemCount: gridSize * gridSize,
+              itemBuilder: (context, index) {
+                int row = index ~/ gridSize;
+                int col = index % gridSize;
+                return Container(
+                  margin: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Text(
+                      grid[row][col] == 0 ? '' : grid[row][col].toString(),
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 20),
+          // Directional buttons
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_upward),
+                    iconSize: 40,
+                    onPressed: moveUp,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    iconSize: 40,
+                    onPressed: moveLeft,
+                  ),
+                  SizedBox(width: 20),
+                  IconButton(
+                    icon: Icon(Icons.arrow_forward),
+                    iconSize: 40,
+                    onPressed: moveRight,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 40,
+                    onPressed: moveDown,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
